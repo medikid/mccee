64969cc0492aa52e6fe4239b0dd5a6b9a9353aca
medikid <soma.programmer@gmail.com>
2012-10-03 12:23:36 -0700

    Theme implemented

--- sites/all/themes/medikid_blue/shells/QuestionSheetNavigator_shell.php
+++ sites/all/themes/medikid_blue/shells/QuestionSheetNavigator_shell.php
@@ -1,17 +1,17 @@
 <?php
 $tp_total=150;
 $qn=1;
-$qs_nav_items = '<ul class="qs_nav_items" >';
+$qnav_items = '<ul class="qnav_items" >';
 while ($qn <= $tp_total){
 	$mod = $qn % 2;
-	$qs_nav_items .= '<li class="qs_nav_item" id="'.$qn.'" onclick="q_retr(this.id);"><ul ';
+	$qnav_items .= '<li class="qnav_item" id="'.$qn.'" onclick="q_retr(this.id);"><ul ';
 	if ($mod == 0){
-	$qs_nav_items .= 'id="even"';	
-	} else $qs_nav_items .= 'id="odd"';
-	$qs_nav_items .='><li class="fl">fl</li><li class="vw">vw</li><li class="qn">'.$qn.'</li><li class="an">a</li><li class="va">va</li><li class="du">du</li></ul></li>';
+	$qnav_items .= 'id="even"';	
+	} else $qnav_items .= 'id="odd"';
+	$qnav_items .='><li class="fl">fl</li><li class="vw">vw</li><li class="qn">'.$qn.'</li><li class="an">a</li><li class="va">va</li><li class="du">du</li></ul></li>';
 	$qn++;
 }
-$qs_nav_items .='</ul>';
+$qnav_items .='</ul>';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -27,10 +27,10 @@ $qs_nav_items .='</ul>';
 </head>
 <body>
 <div class="user_toolbar">user_toolbar</div>
-<div class="question_sheet_navbar"><?php print $qs_nav_items; ?></div>
+<div class="question_sheet_navbar"><?php print $qnav_items; ?></div>
 <div class="content">
 <div class="question_viewer">question_viewer</div>
-<div class="notes_viwer">notes_viwer</div>
+<div class="notes_viewer">notes_viwer</div>
 </div>
 </boody>
 </html>
