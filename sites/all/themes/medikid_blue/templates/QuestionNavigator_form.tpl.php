42881ad96b2cf416e03984a7db25b8a06dadce20
medikid <soma.programmer@gmail.com>
2012-10-10 12:37:10 -0700

    DB Normalization

--- sites/all/themes/medikid_blue/templates/QuestionNavigator_form.tpl.php
+++ sites/all/themes/medikid_blue/templates/QuestionNavigator_form.tpl.php
@@ -3,7 +3,7 @@
 <body>
     This is Question Navigator Form theme/templates folder
 // Search textfield
-<?php print $mcq_viewer; ?>
+<?php print $mcq_viewer; unset($sidebar_first);?>
 
 // this is must
 <?php print $form_extras; ?>
